b96cd37f896393b33110bdd00250c3b441fd740f
Abedin Abedin <abedin@vicadmins-macbook-pro-2.ds.nrc.ca>
2018-11-22 13:02:20 -0800

    MOID subroutine v.1 20181122

--- moid2.for
+++ moid2.for
@@ -1,34 +1,52 @@
         implicit none
 
-        integer i,j,k,l,indmin,p,i1,i2,invmod,ddm,ddm2
-        real*8, allocatable, dimension(:):: truan1,truan2
-        real*8 semi1,ecc1,incl1,aper1,lasc1,semi2,ecc2,incl2,aper2,lasc2
-        real*8, allocatable,dimension(:):: moid,x1,y1,z1,x2,y2,z2
-        real*8 meanan1, meanan2,x,y,z,r1,r2
-        real*8, allocatable, dimension(:) :: dist
-        real*8 pi,d2r,r2d,truan,m1,m2,m1min,m1max,m2min,m2max,dm1,dm2
-        external invmod
-
-        ddm = 100
-        ddm2=ddm*ddm
+        real*8 pi,r2d,d2r
+        real*8 semi1,ecc1,incl1,aper1,lasc1
+        real*8 semi2,ecc2,incl2,aper2,lasc2
+        real*8 meanan1,meanan2,truan1,truan2,moid
 
         pi = 4.0*atan(1.d0)
         r2d = 180./pi
         d2r = 1./r2d
+        
+        semi1 = 3.09197d0
+        ecc1 = 0.68557d0
+        incl1 = 11.23697*d2r
+        lasc1 = 69.6622d0*d2r
+        aper1 = 199.386d0*d2r
+        meanan1 = 354.77*d2r
 
-        semi1 = 1.44270708d0
-        ecc1 = 0.483925457d0
-        incl1 = 4.098859d0*d2r
-        lasc1 = 213.749769d0*d2r
-        aper1 = 102.916097d0*d2r
-        meanan1 = 214.3092098d0*d2r
+        semi2 = 1.00045d0
+        ecc2 = 0.0171187d0
+        incl2 = 0.000418817*d2r
+        lasc2 = 135.083*d2r
+        aper2 = 326.726d0*d2r
+        meanan2 = 130.608d0*d2r        
+        
+        call FIND_MOID(semi1,ecc1,incl1,lasc1,aper1,meanan1,
+     %                  semi2,ecc2,incl2,lasc2,aper2,meanan2,
+     %                  truan1,truan2,moid)
 
-        semi2 = 5.2034d0
-        ecc2 = 0.0483d0
-        incl2 = 1.305d0*d2r
-        lasc2 = 100.556d0*d2r
-        aper2 = 274d0*d2r
-        meanan2 = 2.55767d0*d2r
+        print*,truan1*r2d,truan2*r2d,moid
+        end
+
+        SUBROUTINE FIND_MOID(semi1,ecc1,incl1,lasc1,aper1,meanan1,
+     %                   semi2,ecc2,incl2,lasc2,aper2,meanan2,
+     %                   ttan1,ttan2,mmoid) 
+        implicit none
+
+        integer i,j,k,l,indmin,p,i1,i2,invmod,ddm,ddm2
+        real*8, allocatable, dimension(:):: truan1,truan2
+        real*8 semi1,ecc1,incl1,aper1,lasc1,semi2,ecc2,incl2,aper2,lasc2
+        real*8, allocatable,dimension(:):: moid,x1,y1,z1,x2,y2,z2
+        real*8 meanan1, meanan2,x,y,z,r1,r2,tan1(4),tan2(4)
+        real*8, allocatable, dimension(:) :: dist
+        real*8 pi,d2r,r2d,truan,m1,m2,m1min,m1max,m2min,m2max,dm1,dm2
+        real*8 ttan1, ttan2, mmoid
+        external invmod
+
+        ddm = 100
+        ddm2=ddm*ddm
 
         allocate(truan1(ddm))
         allocate(truan2(ddm))
@@ -61,20 +79,19 @@
 
               dist(k) = sqrt((x1(i)-x2(j))**2 + (y1(i)-y2(j))**2 +
      %                  (z1(i)-z2(j))**2)
-c              print*,k,r1,r2
-c     %           truan2(j)*r2d,dist(k)
             end do
           end do
           call FindInVector(ddm2,dist.eq.minval(dist),p)
           i1 = invmod(ddm,p,ddm)
           i2 = mod(p,ddm)
           moid(l)=minval(dist)
-          print*,truan1(i1),truan2(i2),moid(l)
+          tan1(l) = truan1(i1)
+          tan2(l) = truan2(i2)
           call t2m(truan1(i1-1),ecc1,m1min)
           call t2m(truan1(i1+1),ecc1,m1max)
           call t2m(truan2(i2-1),ecc2,m2min)
           call t2m(truan2(i2+1),ecc2,m2max)
-c          print*,m1min,m1max
+
           if (m1min.gt.m1max) then
             dm1 = (-m1max + m1min)/ddm
           else
@@ -87,7 +104,15 @@ c          print*,m1min,m1max
           end if
 
         end do
-
+        call FindInVector(4,moid.eq.minval(moid),p)
+c        print*,i2
+c        write(6,"(/,'TAmoid_body1',5x,'TAmoid_body2',5x,'MOID' )")
+c        write(6,"('.................................................')")
+c        write(6,"(1p,E12.5,5x,1p,E12.5,4x,1p,E15.8,1x,'AU',/,/)")
+c     %        tan1(p),tan2(p),minval(moid)
+        ttan1=tan1(p)
+        ttan2=tan2(p)
+        mmoid=minval(moid)
         end
 
         SUBROUTINE KEP2CART(semi,ecc,incl,aper,lasc,man,x,y,z,tanom)
